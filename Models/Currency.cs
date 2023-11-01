747c6aa22355c1e487df5f985500ef6aa05d098b
b6land <20431288+b6land@users.noreply.github.com>
2023-11-01 22:23:16 +0800

    docs: add comments in currency class

--- Models/Currency.cs
+++ Models/Currency.cs
@@ -1,7 +1,16 @@
 namespace CurrencyConvert.Models;
 
+/// <summary>
+/// 幣別轉換結果類別
+/// </summary>
 public class Currency
 {
+    /// <summary>
+    /// 轉換結果
+    /// </summary>
     public string? msg { get; set; }
+    /// <summary>
+    /// 轉換後金額
+    /// </summary>
     public string? amount { get; set; }
 }
